d070df6ac2510fc4fdf54575bec8a2759e3fe654
Lateef Azeez <codelate18@gmail.com>
2022-03-12 21:21:06 -0700

    replace react codes

---
+++ frontend_react/src/container/About/About.jsx
@@ -0,0 +1,14 @@
+import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
+
+import "./About.scss";
+
+export default function About() {
+  return (
+    <>
+      <h2 className="heade-text">
+        I know That <span>Good Design</span> means <span>Good Business</span>
+      </h2>
+    </>
+  );
+}
