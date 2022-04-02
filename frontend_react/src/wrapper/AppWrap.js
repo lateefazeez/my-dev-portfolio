e6d8c5954a851d909ea1b5c529690ea0cf1a5bc1
Lateef Azeez <codelate18@gmail.com>
2022-04-02 13:34:12 -0600

    add footer section

--- frontend_react/src/wrapper/AppWrap.js
+++ frontend_react/src/wrapper/AppWrap.js
@@ -9,8 +9,8 @@ const AppWrap = (Component, idName, classNames) =>
         <div className="app__wrapper app__flex">
           <Component />
           <div className="copyright">
-            <p className="p-text">@2020 LateefAzeez</p>
-            <p className="p-text">All rights reserved</p>
+            <p className="p-text">@2022 LateefAzeez</p>
+            <p className="p-text">All Rights Reserved</p>
           </div>
         </div>
         <NavigationDots active={idName} />
