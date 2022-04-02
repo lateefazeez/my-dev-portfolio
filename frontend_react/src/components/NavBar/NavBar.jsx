2081606cab8eeae675d0f25dfac116552e4057e7
Lateef Azeez <codelate18@gmail.com>
2022-04-02 17:47:47 -0600

    test update to nav

--- frontend_react/src/components/NavBar/NavBar.jsx
+++ frontend_react/src/components/NavBar/NavBar.jsx
@@ -15,14 +15,19 @@ export default function NavBar() {
         <img src={images.logo} alt="page logo" />
       </div>
       <ul className="app__navbar-links">
-        {["home", "about", "work", "skills", "testiomonials", "contact"].map(
-          (item) => (
-            <li className="app__flex p-text" key={`link-${item}`}>
-              <div />
-              <a href={`#${item}`}>{item}</a>
-            </li>
-          )
-        )}
+        {[
+          "homeless",
+          "about",
+          "work",
+          "skills",
+          "testiomonials",
+          "contact",
+        ].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
       </ul>
       <div className="app__navbar-menu">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
